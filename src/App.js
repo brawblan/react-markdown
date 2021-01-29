5b498f2bc092066b0cfd86f05f43445e28e17405
Brandon Blankenstein <blankensteinpercussion@gmail.com>
2021-01-28 23:04:32 -0600

    finished for night, TODO=render default page load

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 const ReactMarkdown = require('react-markdown')
 const markdown = `# hello
@@ -7,11 +7,35 @@ const markdown = `# hello
 1. list item 1
 2. list item 2
   - sublist item 1
+
+> This is a blockquote
+
+Use two asterisks before and after a word/words for **bold** text.
+
+Use backticks to write code \`<div></div>\`
+
+Use three backticks before and after a codeblock
+\`\`\`
+// this is multi-line code
+
+<h1>Hello World!</h1>
+<p>Welcome to my Markdown Editor and Previewer</p>
+something() {
+  if (!idk) {
+    return rad
+  }
+}
+
+\`\`\`
+
+Links are created by adding the link title between square brackets and putting the link inside parentheses next to it [] () [FCC](https://www.freecodecamp.com)
+
+Images are added by using the same square bracket and parentheses as with link links, but adding an exclamation point before
+![Random Pic](https://pixabay.com/images/search/flower/)
 `
 
 
-function Editor() {
-  const [editor, setPreview] = useState();
+const Editor = ({setPreview}) => {
 
   const handleChange = (e) => {
     setPreview(e.currentTarget.value)
@@ -19,31 +43,39 @@ function Editor() {
   
   return (
     <div>
-
       <h2>Editor</h2>
       <textarea id="editor" children={markdown} onChange={handleChange} />
-
-      <h2>Preview</h2>
-      <div id="preview">
-        <ReactMarkdown>
-            {editor}
-          </ReactMarkdown>
-      </div>
-
     </div>
   )
 }
 
-// function Preview() {
-//   return (
-//   )
-// }
+const Preview = ({editor}) => {
+  return (
+    <div>
+      <h2>Preview</h2>
+      <div id="preview">
+        <ReactMarkdown>
+          {editor}
+        </ReactMarkdown>
+      </div>
+    </div>
+  )
+}
 
 function App() {
+  const [editor, setPreview] = useState();
+  useEffect(() => {
+    setPreview(editor);
+  }, [editor])
   return (
     <div className="app">
-      <Editor />
-      {/* <Preview /> */}
+      <Editor setPreview={setPreview} />
+      <Preview 
+        editor={editor} 
+        dangerouslySetInnerHTML={{
+          __html: {setPreview}
+        }}  
+        />
     </div>
   ); 
 }
